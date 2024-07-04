fix: fix missing gradient accumulation loss scaling, return bias to some linear layers

supervoice_valle/model_nar.py
@@ -44,7 +44,8 @@ class SupervoceNARModel(torch.nn.Module):
         torch.nn.init.normal_(self.codec_index_embedding.weight, mean=0.0, std=0.02)
 
         # Output prediction
-        self.prediction = torch.nn.Linear(self.n_dim, 1024, bias=False)
+        # self.prediction = torch.nn.Linear(self.n_dim, 1024, bias=False)
+        self.prediction = torch.nn.Linear(self.n_dim, 1024)
     
     def forward(self, *, condition_text, condition_audio, audio, codec, loss = False):
 

supervoice_valle/transformer.py
@@ -92,7 +92,8 @@ class AttentionBlock(torch.nn.Module):
         # self.attention_dropout = nn.Dropout(att_dropout)
 
         # Output flatten multiple heads into single tensor
-        self.attention_output = nn.Linear(n_dim_head * n_heads, n_dim, bias=False)
+        # self.attention_output = nn.Linear(n_dim_head * n_heads, n_dim, bias=False)
+        self.attention_output = nn.Linear(n_dim_head * n_heads, n_dim)
         torch.nn.init.normal_(self.attention_output.weight, mean=0.0, std=0.02)
 
         # Attention dropout

train.py
@@ -32,7 +32,7 @@ from supervoice_valle import SupervoceNARModel, Tokenizer
 from train.dataset import load_sampler, create_async_loader
 
 # Train parameters
-train_experiment = "valle-27"
+train_experiment = "valle-30"
 train_project="supervoice-valle"
 train_auto_resume = True
 
@@ -81,8 +81,8 @@ def main():
     accelerator.print("Loading dataset...")
     tokenizer = Tokenizer("./tokenizer_text.model")
     # train_sampler = load_sampler("./external_datasets/libriheavy/libriheavy_cuts_medium.jsonl.gz", "./external_datasets/libriheavy-medium-encodec/", train_batch_size, tokenizer)
-    # train_sampler = load_sampler("./external_datasets/libriheavy/libriheavy_cuts_large.jsonl.gz", "./external_datasets/libriheavy-large-encodec/", train_batch_size, tokenizer)
-    train_sampler = load_sampler("./external_datasets/libriheavy/libriheavy_cuts_small.jsonl.gz", "./external_datasets/libriheavy-encodec/", train_batch_size, tokenizer)
+    train_sampler = load_sampler("./external_datasets/libriheavy/libriheavy_cuts_large.jsonl.gz", "./external_datasets/libriheavy-large-encodec/", train_batch_size, tokenizer)
+    # train_sampler = load_sampler("./external_datasets/libriheavy/libriheavy_cuts_small.jsonl.gz", "./external_datasets/libriheavy-encodec/", train_batch_size, tokenizer)
     train_loader = create_async_loader(train_sampler, num_workers = train_loader_workers)
     train_cycle = cycle(train_loader)
 
@@ -215,6 +215,9 @@ def main():
                         codec = audio_codecs,
                         loss = True
                     )
+
+                    # Rescale loss
+                    loss = loss / train_grad_accum_every
                     
                     # Check if loss is NaN
                     # if torch.isnan(loss):